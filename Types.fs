4b4ce1210b8aa8acd6957c9bb488c5c69047d62a
Eji1700 <regulus87@yahoo.com>
2024-10-27 20:20:01 -0700

    Some basic type mockups

--- Types.fs
+++ Types.fs
@@ -1,7 +1,18 @@
 module Types 
 open System
 
+type Protocol =
+    {   ID: Guid
+        Name: string
+        Description: string
+        Focus: string }
+
+type Effect = string
+type Command = Effect list
 type Card =
-    {
-        ID: Guid
-    }
+    {   ID: Guid
+        Protocol: Protocol
+        Value: int
+        TopCommand:Command
+        MiddleCommand:Command
+        BottomCommand:Command }
